3bae43fc0f71fd30d260a844a65fef4bc8b03413
oikomi <miaohonghit@gmail.com>
2015-11-30 11:17:13 +0800

    init

--- src/main/java/com/baidu/uaq/pagespeed/config/Const.java
+++ src/main/java/com/baidu/uaq/pagespeed/config/Const.java
@@ -4,7 +4,7 @@ package com.baidu.uaq.pagespeed.config;
  * Created by miaohong01 on 15/11/9.
  */
 public class Const {
-    public static final String PAGE_SPEED_BIN = "bin/pagespeed_bin";
+    public static final String PAGE_SPEED_BIN = "bin/pagespeed_bin_2";
     public static final String BASE_PATH = "/tmp/pagespeed/";
     public static final String HAR_PREFIX = "har_";
     public static final String RESULT_PREFIX = "json_";
